3fe7acdf2718b5ee41d5a1cee97302f697ea3fdb
Betty-edit <betelhemgezahegn45@gmail.com>
2023-06-22 18:58:19 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _isdigit - check if numbers are 0-9
- * @c: char to be checked
- *
- * Return: 0 or 1
+ * _isdigit - checks for a digit
+ * @c: character to be checked
+ * Return: 1 for digit character or 0 for anything else
  */
 int _isdigit(int c)
 {
-	if (c <= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,26 @@
  */
 void print_triangle(int size)
 {
+	int row, hash, space;
+
 	if (size <= 0)
 	{
 		putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
+		for (row = 1; row <= size; row++)
 		{
-
-			for (j = 1; j < size; j++)
+			for (space = size - row; space >= 1; space--)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
-			for (j = 1; j <= i; j++)
+			for (hash = 1; hash <= row; hash++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
 			putchar('\n');
 		}
 	}
 }
+
