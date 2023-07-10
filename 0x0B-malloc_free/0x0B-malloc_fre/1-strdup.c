ba9f180bf9707cf444a42bd688fb7de7d9533839
Betty-edit <betelhemgezahegn45@gmail.com>
2023-07-10 01:52:41 -0700

    0x0b malloc free

---
+++ 0x0B-malloc_free/0x0B-malloc_fre/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - dublicate to new memory space location
+ * @str: string to dublicate
+ *
+ * Return: the string duplicated
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 1;
+       	int a = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+       s = malloc((sizeof(char) * i) + 1);
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = '\0';
+
+	return (s);
+}
